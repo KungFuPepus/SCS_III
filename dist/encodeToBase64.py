bf1b062e536fe50bdfbc2a1bd4223ce39142451f
KungFuPepus <krystian.fedor2@onet.pl>
2025-05-11 23:00:44 +0200

    Bonkers

--- dist/encodeToBase64.py
+++ dist/encodeToBase64.py
@@ -1,3 +1,9 @@
 import base64
-with open("WindowsUpdate.exe", "rb") as f:
-    print(base64.b64encode(f.read()).decode("utf-8"))
+
+# Read the executable file
+with open("FriendlyMalware.exe", "rb") as f:
+    encoded_data = base64.b64encode(f.read()).decode("utf-8")
+
+# Write the Base64 string to a text file
+with open("encoded_payload.txt", "w") as f:
+    f.write(encoded_data)
